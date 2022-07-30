91db01753cc16a154c439b8056c831e53a56a1cb
5544451 <62584670+5544451@users.noreply.github.com>
2022-07-31 01:55:51 +0900

    카카오 로그인 세션저장

---
+++ jejuPrj/src/main/java/com/jeju/web/kakaolog.java
@@ -0,0 +1,41 @@
+package com.lowCO2.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/kakaolog")
+public class kakaolog extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws IOException, ServletException
+    {
+		//쓰고 읽기에 한글가능하도록 ㅇㅣㄴ코딩
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		HttpSession session = req.getSession();
+		
+		
+		String id = req.getParameter("kakaoN");
+		session.setAttribute("kakaoN", id);
+
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/mainpage.jsp");
+		dispatcher.forward(req, response);
+		
+    }
+}
