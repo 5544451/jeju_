38e6a1cdb33f92f66c53798e2168fe5f6f0dab96
5544451 <62584670+5544451@users.noreply.github.com>
2022-07-31 01:59:36 +0900

    데베 클래스

---
+++ jejuPrj/src/main/java/com/jeju/web/position.java
@@ -0,0 +1,9 @@
+package com.lowCO2.web;
+
+public class position {
+    public String name;
+    public String roadAddress;
+    public String lotAddress;
+    public Double latitude;
+    public Double longitude;
+}
