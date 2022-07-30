3d9373164781c94c48ae42b1fc5fd6c256e6bd6c
5544451 <62584670+5544451@users.noreply.github.com>
2022-07-31 01:54:29 +0900

    로그인오류고침

--- jejuPrj/src/main/java/com/jeju/web/Log.java
+++ jejuPrj/src/main/java/com/jeju/web/Log.java
@@ -1,28 +1,51 @@
-package com.jeju.web;
+package com.lowCO2.web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
 
-@WebServlet("/login")
-public class Log extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+@WebServlet("/mainpage")
+public class LogIn extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws IOException, ServletException
+    {
+		//쓰고 읽기에 한글가능하도록 ㅇㅣㄴ코딩
 		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charest=UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		String[] mem = new String[5];
+		HttpSession session = req.getSession();
 		
-		String id = request.getParameter("user-id");
-		String pwd = request.getParameter("user-pwd");
 		
-		String kakao =  "";
-		String naver = "";		
+		String id = req.getParameter("user-id");
+		String psd = req.getParameter("user-pwd");
 		
-		kakao = request.getParameter("kakaonickname");
-		naver = request.getParameter("navernickname");
+		DBControll DBcon = new DBControll();
+		mem = DBcon.selectOne(id, "member");
+
+		//쿼리문 준비
+
+		if(psd.equals(mem[2]))
+		{
+			session.setAttribute("comN", id);
+		}
 		
-		response.getWriter().printf(id);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/mainpage.jsp");
+		dispatcher.forward(req, response);
 		
-	}
+    }
 }
